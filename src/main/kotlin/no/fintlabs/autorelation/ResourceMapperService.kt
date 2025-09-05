476bbc4df217293950daf8abbd4b0e81aa050c51
Hknots <hknots99@gmail.com>
2025-09-05 14:32:22 +0200

    feat: add ResourceMapperService

---
+++ src/main/kotlin/no/fintlabs/autorelation/ResourceMapperService.kt
@@ -0,0 +1,28 @@
+package no.fintlabs.autorelation
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.fint.model.resource.FintResource
+import no.fintlabs.autorelation.kafka.model.ResourceType
+import no.fintlabs.metamodel.MetamodelService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class ResourceMapperService(
+    private val metamodelService: MetamodelService,
+    private val objectMapper: ObjectMapper
+) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun mapResource(resourceType: ResourceType, resourceObject: Any): FintResource? =
+        metamodelService.getResource(resourceType.domain, resourceType.pkg, resourceType.resource)
+            ?.let { mapResource(resourceObject, it.resourceType) }
+            ?.onFailure { logger.error("Failed mapping resource: ${resourceType.resource}: ${it.message}") }
+            ?.getOrNull()
+
+    private fun mapResource(resourceObject: Any, resourceType: Class<out FintResource>): Result<FintResource> =
+        runCatching { objectMapper.convertValue(resourceObject, resourceType) }
+            .onFailure { logger.error("AAA: ${it.message}") }
+
+}
