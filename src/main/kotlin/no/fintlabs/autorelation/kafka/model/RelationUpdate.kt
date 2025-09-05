30d9c67a40083da8cadf2d23aa83d424ddc46f93
Hknots <hknots99@gmail.com>
2025-09-05 14:32:50 +0200

    feat: add companion object to RelationUpdate

--- src/main/kotlin/no/fintlabs/autorelation/kafka/model/RelationUpdate.kt
+++ src/main/kotlin/no/fintlabs/autorelation/kafka/model/RelationUpdate.kt
@@ -1,9 +1,22 @@
 package no.fintlabs.autorelation.kafka.model
 
+import no.fintlabs.autorelation.cache.RelationSpec
+
 data class RelationUpdate(
     val orgId: String,
     val domainName: String,
     val packageName: String,
     val resource: ResourceRef,
     val relation: RelationRef
-)
+) {
+    companion object {
+        fun from(orgId: String, relationSpec: RelationSpec, resourceId: ResourceId, relationIds: List<ResourceId>) =
+            RelationUpdate(
+                orgId = orgId,
+                domainName = relationSpec.resourceType.domain,
+                packageName = relationSpec.resourceType.pkg,
+                resource = ResourceRef(relationSpec.resourceType.resource, resourceId),
+                relation = RelationRef(relationSpec.inversedRelation.name, relationIds)
+            )
+    }
+}
