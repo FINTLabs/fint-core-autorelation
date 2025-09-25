4ba4a3ef34e05ca5204e8cddf3951491c61d7e1b
Hknots <hknots99@gmail.com>
2025-09-25 11:11:58 +0200

    test: add ResourceUtils to generate resources

---
+++ src/test/integration/kotlin/no/fintlabs/autorelation/ResourceUtils.kt
@@ -0,0 +1,28 @@
+package no.fintlabs.autorelation
+
+import no.fint.model.felles.kompleksedatatyper.Identifikator
+import no.fint.model.resource.Link
+import no.fint.model.resource.utdanning.vurdering.ElevfravarResource
+import no.fint.model.resource.utdanning.vurdering.FravarsregistreringResource
+
+fun createElevfravarResource(id: String = "123", vararg elevforholdIds: String) =
+    ElevfravarResource().apply {
+        systemId = Identifikator().apply {
+            identifikatorverdi = id
+        }
+
+        for (elevforholdId in elevforholdIds) {
+            addElevforhold(Link.with("systemid/$elevforholdId"))
+        }
+    }
+
+fun createFravarsregistreringResource(resourceId: String = "123", vararg elevfravarIds: String) =
+    FravarsregistreringResource().apply {
+        systemId = Identifikator().apply {
+            identifikatorverdi = resourceId
+        }
+
+        for (elevfravarId in elevfravarIds) {
+            addElevfravar(Link.with("systemid/$elevfravarId"))
+        }
+    }
