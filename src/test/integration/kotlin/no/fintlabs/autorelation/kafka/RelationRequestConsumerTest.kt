edeff787a2db9e3811dae10b4db3d748f3e13d77
Hknots <hknots99@gmail.com>
2025-09-25 15:34:57 +0200

    test: create RelationRequestConsumer integration test

--- src/test/integration/kotlin/no/fintlabs/autorelation/kafka/RelationRequestConsumerTest.kt
+++ src/test/integration/kotlin/no/fintlabs/autorelation/kafka/RelationRequestConsumerTest.kt
@@ -1,9 +1,61 @@
 package no.fintlabs.autorelation.kafka
 
-import no.fintlabs.autorelation.utils.KafkaUtils
+import no.fintlabs.autorelation.AutoRelationService
+import no.fintlabs.autorelation.kafka.producer.RelationRequestProducer
+import no.fintlabs.autorelation.model.RelationOperation
+import no.fintlabs.autorelation.model.RelationRequest
+import no.fintlabs.autorelation.model.ResourceType
+import org.awaitility.Awaitility.await
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.reset
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean
+import java.util.concurrent.TimeUnit
 
+@SpringBootTest
+@EmbeddedKafka(partitions = 1, controlledShutdown = true, count = 2)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class RelationRequestConsumerTest @Autowired constructor(
-    private val kafkaUtils: KafkaUtils
+    private val relationRequestProducer: RelationRequestProducer
 ) {
+
+    @MockitoSpyBean
+    private lateinit var autoRelationService: AutoRelationService
+
+    @MockitoSpyBean
+    private lateinit var relationRequestConsumer: RelationRequestConsumer
+
+    @BeforeEach
+    fun setup() {
+        reset(autoRelationService, relationRequestConsumer)
+    }
+
+    @Test
+    fun `process message that matches topic`() {
+        val relationRequest = createRelationRequest()
+
+        relationRequestProducer.produceEvent(relationRequest)
+
+        await().atMost(10, TimeUnit.SECONDS).untilAsserted {
+            verify(relationRequestConsumer, times(1)).consumeRecord(any())
+            verify(autoRelationService, times(1)).processRequest(relationRequest)
+        }
+    }
+
+    private fun createRelationRequest() =
+        RelationRequest(
+            type = ResourceType("utdanning", "vurdering", "elevfravar"),
+            orgId = "fintlabs.no",
+            resource = "anything",
+            operation = RelationOperation.DELETE,
+            entityRetentionTime = null
+        )
+
 }
