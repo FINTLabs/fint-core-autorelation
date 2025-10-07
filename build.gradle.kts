97be9a79a912a111dfe2e3c163f13f66d887cb20
Hknots <hknots99@gmail.com>
2025-10-07 11:16:03 +0200

    build: upgrade lib version & remove unused metamodel

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,7 @@ dependencies {
 
     implementation("org.springframework.kafka:spring-kafka")
     implementation("no.fintlabs:fint-kafka:3.2.0-rc-3")
-    implementation("no.fintlabs:fint-core-consumer-metamodel:2.0.0-rc-4")
-    implementation("no.fintlabs:fint-core-autorelation-lib:2.0.0-rc-2")
+    implementation("no.fintlabs:fint-core-autorelation-lib:2.0.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
