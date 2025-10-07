906dca27fdfd0663d3e22e9141ff3b763a66f5df
Hknots <hknots99@gmail.com>
2025-10-07 11:30:21 +0200

    refactor: use correct field

--- src/main/kotlin/no/fintlabs/autorelation/kafka/mapper/RelationUpdateMapper.kt
+++ src/main/kotlin/no/fintlabs/autorelation/kafka/mapper/RelationUpdateMapper.kt
@@ -30,7 +30,7 @@ class RelationUpdateMapper {
                 name = relationSpec.inversedRelation.name,
                 ids = relationIds
             ),
-            entityCreatedTime = request.entityRetentionTime
+            entityCreatedTime = request.entityCreatedTime
         )
 
 }
